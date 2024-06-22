cc0500fd49599f15ff448b59b087ad691391f8e0
Angel Vera <ceolitzin@protonmail.com>
2024-06-22 09:36:47 -0600

    Curso JavaScript Youtube con SuperSimpleDev por que el de Platzi apesta

---
+++ Javascript/HolaMundo/TiposdeDatos/tipodedatos.js
@@ -0,0 +1,28 @@
+// Tipo de dato primitivo - Inumtable
+
+let numero = 23
+numero = numero + 521
+console.log(numero)
+
+let esVerdadero = true
+esVerdadero = false
+console.log(esVerdadero)
+
+// Tipo de dato complejo - mutable
+
+let usuario = {nombre: 'Pepito', edad: 15}
+usuario.edad = 20
+console.log(usuario)
+
+let frutas = ['manzana', 'pera']
+frutas[0] = 'sandia'
+console.log(frutas)
+
+function cambiarNombre (objeto){
+    objeto.nombre = 'Nuevo nombre'
+}
+
+let persona = {nombre: 'Antonio'}
+cambiarNombre(persona)
+
+console.log(persona)
---
+++ Javascript/Variables/variables.js
@@ -0,0 +1,14 @@
+let cajaDeAndy = 'Woody'
+console.log(cajaDeAndy)
+
+// No permitido
+
+let c = 'woody' //utilizar variables más descriptivas
+let cda = 'woody'
+let pcandy = 'woody'
+
+// Mejores prácticas
+
+let urlDelUsuario = 'https://wwww.google.com'
+let idDelUsuario = '234135'
+
